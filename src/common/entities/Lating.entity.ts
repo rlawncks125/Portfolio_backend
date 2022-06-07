990cfba8b226c4d729a28749fecb955a76c8ed18
김주찬 <rlawncks125@naver.com>
2022-06-08 02:02:11 +0900

    Lating type 변경

--- src/common/entities/Lating.entity.ts
+++ src/common/entities/Lating.entity.ts
@@ -2,11 +2,11 @@ import { ApiProperty } from '@nestjs/swagger';
 import { Column } from 'typeorm';
 
 export class Lating {
-  @Column({ type: 'double' })
+  @Column({ type: 'float8' })
   @ApiProperty({ description: 'Lating_X', example: 'X 좌표입니다.' })
   x: number;
 
-  @Column({ type: 'double' })
+  @Column({ type: 'float8' })
   @ApiProperty({ description: 'Lating_Y', example: 'Y 좌표입니다.' })
   y: number;
 }
